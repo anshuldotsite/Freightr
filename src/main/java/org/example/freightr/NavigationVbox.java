30b85a6294b4b1a3e8541d0749baf2757e022d74
Kohinoor Jeet Singh <w0859774@myscc.ca>
2024-10-25 16:11:01 -0400

    #22 made new class for vbox

---
+++ src/main/java/org/example/freightr/NavigationVbox.java
@@ -0,0 +1,33 @@
+package org.example.freightr;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class NavigationVbox extends VBox {
+
+    private Stage stage;
+
+    public NavigationVbox(Stage stage) {
+        this.stage = stage;
+
+        Button Button1 = new Button("Parcel Scene Button");
+        Button button2 = new Button("dummy button");
+        Button button3 = new Button("dummy button");
+        Button button4 = new Button("dummy button");
+        Button1.setOnAction(e-> System.out.println("print this"));
+
+        button2.setOnAction(e-> System.out.println("print this"));
+
+        this.setPadding(new Insets(20));
+        this.setSpacing(15);
+        this.setStyle("-fx-background-color: lightgray;");
+
+
+        this.getChildren().addAll(Button1,button2,button3,button4);
+    }
+
+
+
+}
