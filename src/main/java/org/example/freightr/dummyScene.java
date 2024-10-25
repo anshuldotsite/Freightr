feef08d76ed42f9ec24a30a21508177a2d7100e5
Kohinoor Jeet Singh <w0859774@myscc.ca>
2024-10-25 16:34:45 -0400

    just added some more functional code * started working on the packageform

---
+++ src/main/java/org/example/freightr/dummyScene.java
@@ -0,0 +1,31 @@
+package org.example.freightr;
+
+
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class dummyScene {
+
+
+
+
+    public static Scene CreatedummyScene(Stage stage) {
+        NavigationVbox navigationVbox=new NavigationVbox(stage);
+
+        
+
+
+
+
+        BorderPane root = new BorderPane();
+        root.setLeft(navigationVbox);
+
+        return new Scene(root,900,640);
+
+
+    }
+
+
+
+}
