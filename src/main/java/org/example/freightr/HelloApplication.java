93aeb227c1d15b48cf5e3531260d5b64be89132b
kautukk <w0844992@myscc.ca>
2024-11-08 16:03:32 -0500

    test commit

--- src/main/java/org/example/freightr/HelloApplication.java
+++ src/main/java/org/example/freightr/HelloApplication.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        //logic for db form
         Scene dbScene = DbForm.CreateDBFormScene(stage);
         Scene loginScene = LoginPageScene.createLoginPage(stage);
 
