56d51eb18d36ee86182e0690a52d04c8f944f512
kautukk <w0844992@myscc.ca>
2024-11-08 02:11:56 -0500

    #40 logic to open db form only if credentials txt is missing added

--- src/main/java/org/example/freightr/HelloApplication.java
+++ src/main/java/org/example/freightr/HelloApplication.java
@@ -5,14 +5,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Scene scene = DbForm.CreateDBFormScene(stage);
-        stage.setTitle("Create an account");
-        stage.setScene(scene);
+        Scene dbScene = DbForm.CreateDBFormScene(stage);
+        Scene loginScene = LoginPageScene.createLoginPage(stage);
+
+        File file = new File("credentials.txt");
+        if (!file.exists()){
+            stage.setScene(dbScene);
+        }else {
+            stage.setScene(loginScene);
+        }
         stage.show();
     }
     public static void main(String[] args) {
