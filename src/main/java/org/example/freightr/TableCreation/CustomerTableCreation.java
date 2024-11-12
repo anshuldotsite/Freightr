a6411b3e8adcbc11bcc63eb75b08eecc6beceabd
Kohinoor Jeet Singh <w0859774@myscc.ca>
2024-11-11 21:26:25 -0500

    finished table read code * still need to do the creation fro customer in database with condition

--- src/main/java/org/example/freightr/TableCreation/CustomerTableCreation.java
+++ src/main/java/org/example/freightr/TableCreation/CustomerTableCreation.java
@@ -1,4 +1,100 @@
 package org.example.freightr.TableCreation;
 
-public class CustomerTableCreation {
+
+import org.example.freightr.Database;
+
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import static org.example.freightr.TableCreation.Tableconst.*;
+
+public class CustomerTableCreation implements CustomerDoa {
+
+    private static CustomerTableCreation instance;
+
+
+    private final Database db = Database.getInstance();
+
+    private CustomerTableCreation() {
+
+    }
+
+    public static synchronized CustomerTableCreation getInstance() {
+        if (instance == null) {
+            instance = new CustomerTableCreation();
+        }
+        return instance;
+    }
+
+    @Override
+    public ArrayList<Customer> getAllCustomers() {
+        String query = "SELECT * FROM " + TABLE_CUSTOMER;
+        ArrayList<Customer> customers = new ArrayList<>();
+
+        try {
+            Statement getCustomers = db.getConnection().createStatement();
+            ResultSet resultSet = getCustomers.executeQuery(query);
+
+            while (resultSet.next()) {
+                customers.add(new Customer(
+                        resultSet.getInt(CUSTOMER_COLUMN_ID),
+                        resultSet.getInt(CUSTOMER_COLUMN_COMPANY_ID),
+                        resultSet.getString(CUSTOMER_COLUMN_FIRST_NAME),
+                        resultSet.getString(CUSTOMER_COLUMN_LAST_NAME),
+                        resultSet.getString(CUSTOMER_COLUMN_CONTACT_NUMBER),
+                        resultSet.getString(CUSTOMER_COLUMN_EMAIL),
+                        resultSet.getString(CUSTOMER_COLUMN_ADDRESS),
+                        resultSet.getString(CUSTOMER_COLUMN_ZIPCODE),
+                        resultSet.getString(CUSTOMER_COLUMN_CITY),
+                        resultSet.getString(CUSTOMER_COLUMN_PROVINCE),
+                        resultSet.getString(CUSTOMER_COLUMN_COUNTRY),
+                        resultSet.getString(CUSTOMER_COLUMN_TYPE)
+
+                ));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return customers;
+    }
+
+    @Override
+    public Customer getCustomer(int id) {
+        String query = "SELECT * FROM " + TABLE_CUSTOMER +
+                " WHERE " + CUSTOMER_COLUMN_ID + " = ?";
+        try {
+            PreparedStatement getCustomer = db.getConnection().prepareStatement(query);
+            getCustomer.setInt(1, id);
+            ResultSet data = getCustomer.executeQuery();
+
+            if (data.next()) {
+                return new Customer(
+                        data.getInt(CUSTOMER_COLUMN_ID),
+                        data.getInt(CUSTOMER_COLUMN_COMPANY_ID),
+                        data.getString(CUSTOMER_COLUMN_FIRST_NAME),
+                        data.getString(CUSTOMER_COLUMN_LAST_NAME),
+                        data.getString(CUSTOMER_COLUMN_CONTACT_NUMBER),
+                        data.getString(CUSTOMER_COLUMN_EMAIL),
+                        data.getString(CUSTOMER_COLUMN_ADDRESS),
+                        data.getString(CUSTOMER_COLUMN_ZIPCODE),
+                        data.getString(CUSTOMER_COLUMN_CITY),
+                        data.getString(CUSTOMER_COLUMN_PROVINCE),
+                        data.getString(CUSTOMER_COLUMN_COUNTRY),
+                        data.getString(CUSTOMER_COLUMN_TYPE)
+
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public Customer deleteCustomer(int CustomerID) {
+        return null;
+    }
 }
---
+++ src/main/java/org/example/freightr/kohitest.java
@@ -0,0 +1,17 @@
+package org.example.freightr;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.example.freightr.scenes.PackageFormScene;
+
+public class kohitest extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(PackageFormScene.CreatePackageFormScene(stage));
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+}
