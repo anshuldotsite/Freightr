0c02f09f24ba76cc6cc841ffcdcb6dcbafa75866
kautukk <w0844992@myscc.ca>
2024-11-30 08:40:16 -0500

    #112 added method to retrieve and display specific paclage info with the help of tracking id, made status pojo,doa,and crud method in order to do this

---
+++ src/main/java/org/example/freightr/TableCreation/ObjectClasses/StatusPOJO.java
@@ -0,0 +1,27 @@
+package org.example.freightr.TableCreation.ObjectClasses;
+
+public class StatusPOJO {
+    private int id;
+    private String status;
+
+    public StatusPOJO(int id, String status) {
+        this.id = id;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
