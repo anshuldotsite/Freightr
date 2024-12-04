c6d2ffb864666807e999a4e9657264752e490aa3
kautukk <w0844992@myscc.ca>
2024-12-03 22:14:40 -0500

    #189 DOA for customer city table done

---
+++ src/main/java/org/example/freightr/TableCreation/DOA/CustomerCity.java
@@ -0,0 +1,9 @@
+package org.example.freightr.TableCreation.DOA;
+
+import org.example.freightr.TableCreation.ObjectClasses.CustomerCountByCity;
+
+import java.util.ArrayList;
+
+public interface CustomerCity {
+    public ArrayList<CustomerCountByCity> getCustomerCountByCity();
+}
